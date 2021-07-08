dcd4573ce7ccb4c5f65b01b45c9aa9b9b66fe90a
miknayr <teamry4n@gmail.com>
2021-07-08 12:01:02 -0700

    log in works

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
+import { props } from 'bluebird'
+import { Redirect } from 'react-router-dom'
+import Profile from './Profile'
 
 
-export default function Login() {
+
+export default function Login(props) {
 // state for the controller from
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -19,16 +23,37 @@ export default function Login() {
         email: email,
         password: password
       }
+      console.log('my server url:', process.env.REACT_APP_SERVER_URL)
       const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/login`, requestBody)
 
       console.log(response)
       // save the response to localstorage
 
+      // destructure the response
+      const { token } = response.data
+
+      // save the response to local storage
+      localStorage.setItem('jwtToken', token)
+
+      // decode the jwt token before we put it in state
+      const decoded = jwt.decode(token)
+
+      // set the user in app.js's state
+      props.setCurrentUser(decoded)
+
     } catch (err) {
 
+      if (err.response.status == 400) {
+        setMessage(err.response.data.msg)
+      } else {
+        console.dir(err)
+      }
     }
 
   }
+
+  if(props.currentUser) return <Redirect to='/profile' component={ Profile } currentUser={ props.currentUser } />
+
 
   return (
     <div>
