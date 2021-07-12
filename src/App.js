a82fb26e00e1ad2e88a225c2f3628c55fc0277d8
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-12 16:14:05 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Register from './components/Register.jsx'
 import Welcome from './components/Welcome.jsx'
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
 } from 'react-router-dom'
 
 import {
-  useState,
-  useEffect
+    useState,
+    useEffect
 } from 'react'
 
 import jwt from 'jsonwebtoken'
@@ -29,67 +29,48 @@ function App() {
     const token = localStorage.getItem('jwtToken')
     //if check for token
     if(token) {
-      setCurrentUser(jwt.decode(token))
+        setCurrentUser(jwt.decode(token))
     } else {
-      // else set user in state to be null
-      setCurrentUser(null)
+        // else set user in state to be null
+        setCurrentUser(null)
     }
   }, [])
 
   // function to log the user out
-  const handleLogout = () => {
-    // delete the jwt thats in local storage
-    if(localStorage.getItem('jwtToken')) {
-      localStorage.removeItem('jwtToken')
-      setCurrentUser(null)
+    const handleLogout = () => {
+        if (localStorage.getItem('jwtToken')) {
+            localStorage.removeItem('jwtToken')
+            setCurrentUser(null)
+        }
     }
 
-    // set the user in the state to be null
-
-
-    console.log('log the user out!')
-  }
-
-
   return (
-    <Router>
-      <header>
-        <Navbar  currentUser={ currentUser } handleLogout={ handleLogout } />
-      </header>
-
-
-
-      <div className="App">
-        <Switch>
-
-          <Route 
-            exact path ="/"
-            component={Welcome}
-          />
-
-          <Route 
-            path="/register"
-            render={props => <Register {...props} currentUser={ currentUser } setCurrentUser={setCurrentUser} /> }
-          />    
-          <Route 
-            path="/login"
-            render={props => <Login {...props}  currentUser={ currentUser } setCurrentUser={setCurrentUser} /> }
-          />    
-
-          {/* eventually we will do a conditional render here */}
-          <Route 
-            path="/profile"
-            render={props => currentUser ? <Profile {...props}  currentUser={ currentUser } handleLogout={handleLogout} /> : <Redirect to='/login'/>}
-          />    
-          
-          
-          
-        
-        </Switch>     
-
-      </div>
-    </Router>
-  );
+        <Router>
+            <header>
+                <Navbar  currentUser={currentUser} handleLogout={handleLogout} />
+            </header>
+            <div className="App">
+                <Switch>
+                <Route 
+                    exact path ="/"
+                    component={Welcome}
+                />
+                <Route 
+                    path="/register"
+                    render={props => <Register {...props} currentUser={ currentUser } setCurrentUser={setCurrentUser} /> }
+                />    
+                <Route 
+                    path="/login"
+                    render={props => <Login {...props}  currentUser={ currentUser } setCurrentUser={setCurrentUser} /> }
+                />    
+                <Route 
+                    path="/profile"
+                    render={props => currentUser ? <Profile {...props}  currentUser={ currentUser } handleLogout={handleLogout} /> : <Redirect to='/login'/>}
+                />    
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,91 +1,78 @@
 import { useState } from 'react'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
-import { props } from 'bluebird'
-import { Redirect } from 'react-router-dom'
 import Profile from './Profile'
-
-
 
 export default function Login(props) {
 // state for the controller from
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [message, setMessage] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState('')
 
-  const handleSubmit = async (e) => {
-    try {
-      e.preventDefault()
-      console.log('do axios call')
-  
-      // post to the backend with axios
-      const requestBody = {
-        email: email,
-        password: password
-      }
-      console.log('my server url:', process.env.REACT_APP_SERVER_URL)
-      const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/login`, requestBody)
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault()
+            console.log('do axios call')
+            // post to the backend with axios
+            const requestBody = {
+                email: email,
+                password: password
+            }
+            console.log('my server url:', process.env.REACT_APP_SERVER_URL)
+            const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/login`, requestBody)
 
-      console.log(response)
-      // save the response to localstorage
+            console.log(response)
+            // save the response to localstorage
 
-      // destructure the response
-      const { token } = response.data
+            // destructure the response
+            const { token } = response.data
 
-      // save the response to local storage
-      localStorage.setItem('jwtToken', token)
+            // save the response to local storage
+            localStorage.setItem('jwtToken', token)
 
-      // decode the jwt token before we put it in state
-      const decoded = jwt.decode(token)
+            // decode the jwt token before we put it in state
+            const decoded = jwt.decode(token)
 
-      // set the user in app.js's state
-      props.setCurrentUser(decoded)
-
-    } catch (err) {
-
-      if (err.response.status == 400) {
-        setMessage(err.response.data.msg)
-      } else {
-        console.dir(err)
-      }
+            // set the user in app.js's state
+            props.setCurrentUser(decoded)
+        } catch (err) {
+            if (err.response.status === 400) {
+                setMessage(err.response.data.msg)
+            } else {
+                console.dir(err)
+            }
+        }
     }
-
-  }
 
   if(props.currentUser) return <Redirect to='/profile' component={ Profile } currentUser={ props.currentUser } />
 
-
   return (
-    <div>
-      <h3>Loin to your Account</h3>
-      <p> {message}</p>
-
-      <form onSubmit={handleSubmit}>
-        <label htmlFor={'email-input'}>email:</label>
-          <input 
-            id='email-input'
-            type='email'
-            placeholder='user@domain.com'
-            onChange ={e => setEmail(e.target.value)}
-            value={email}
-          />
-
-          <label htmlFor={'password-input'}> password:</label>
-            <input
-              id='password-input'
-              type='password'
-              placeholder='password'
-              onChange = {e => setPassword(e.target.value)}
-              value={password}
-            />
-
-          <input
-            type='submit'
-            value='login'
-          />
-
-
-      </form>
-    </div>
-  )
+        <div>
+            <h3>Loin to your Account</h3>
+            <p>{message}</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor={'email-input'}>email:</label>
+                <input 
+                    id='email-input'
+                    type='email'
+                    placeholder='user@domain.com'
+                    onChange ={e => setEmail(e.target.value)}
+                    value={email}
+                />
+                <label htmlFor={'password-input'}> password:</label>
+                <input
+                    id='password-input'
+                    type='password'
+                    placeholder='password'
+                    onChange = {e => setPassword(e.target.value)}
+                    value={password}
+                />
+                <input
+                    type='submit'
+                    value='login'
+                />
+            </form>
+        </div>
+    )
 }
