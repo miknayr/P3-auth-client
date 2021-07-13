eae375156573f473b92be9c4a484bdea06049a6a
SpiralV <MVelasco90@hotmail.com>
2021-07-13 14:27:45 -0700

    add events list

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -4,101 +4,19 @@ import axios from 'axios'
 import Login from'./Login'
 import { useState } from 'react'
 export default function Events(props) {
-    // const [eventName, setEventName] = useState('')
-    // const [location, setLocation] = useState('')
-    // const [zoneList, setZoneList] = useState('')
-    const [title, setTitle] = useState('')
-    const [time, setTime] = useState('')
-    const [message, setMessage] = useState('')
 
-    // const addEvent = async (e) => {
-    //     try {
-    //         e.preventDefault()
-    //         const requestBody = { eventName, location, zoneList }
-    //         const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/events`, requestBody)
-
-    // } catch (err){
-    //     console.log(err)        
-    // }}
-
-    const meetup = async (e) => {
-        try {
-            e.preventDefault()
-            const requestBody = { title, time, message }
-            const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/events`, requestBody)
-
-        } catch (err){
-            console.log(err)
-        }
-    }
     return (
-        <div className="new-event">
-            <h3 className="new-event-head">Enter a new event</h3>
-            {/* <form onSubmit={addEvent}>
-                <div>
-                    <input
-                    id='event-input'
-                    type='text'
-                    placeholder='event name'
-                    onChange={(e) => setEventName(e.target.value)}
-                    value={eventName}
-                    />
-                    <input
-                    id='location-input'
-                    type='text'
-                    placeholder='event venue'
-                    onChange={(e) => setLocation(e.target.value)}
-                    value={location}
-                    />
-                    <input
-                    id='zone-input'
-                    type='text'
-                    placeholder='zone name'
-                    onChange={(e) => setZoneList(e.target.value)}
-                    value={zoneList}
-                    />
-                </div>
-                <input type='submit' value='add event' />
-            </form> */}
-                        <form onSubmit={meetup}>
-                <div>
-                    <input
-                        id='title-text'
-                        type='text'
-                        placeholder='enter meetup title'
-                        onChange={(e) => setTitle(e.target.value)}
-                        value={title}
-                        />
-                    <input
-                        id='time-text'
-                        type='time'
-                        placeholder='enter meetup time'
-                        onChange={(e) => setTime(e.target.value)}
-                        value={time}
-                        />
-                    <input
-                        id='message-text'
-                        type='text'
-                        placeholder='enter details here'
-                        onChange={(e) => setMessage(e.target.value)}
-                        value={message}
-                        />
-                </div>
-                <input type='submit' value='Create Meetup' />
-            </form>
-            <ul>
-                <li>
-                    <div>
-                      < input type="checkbox"/>
-                         Adriana is at 8
-                     </div>
-                </li>
-                <li>Terry is at 8</li>
-                <li>Life is Beautiful</li>
-                <li>Day N Vegas</li>
-                <li>iHeartRadio Music Festival</li>
-                <li>Beyond Wonderland</li>
-            </ul>
-        </div>
+    <div>    
+    <h1>Show Checkboxes</h1>
+        <form action="/">
+            <input type="checkbox"/>
+            <label> Jackie at 8PM at Food Trucks B</label><br />
+            <input type="checkbox"/>
+            <label> Terry at 6PM at Stage 1</label><br />
+            <input type="checkbox"/>
+            <label> Ryan at 11PM at Beers</label><br />
+            <input type="submit" value="Submit"/>
+        </form>
+    </div>
     )
 }
