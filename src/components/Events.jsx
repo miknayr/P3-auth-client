401f339fa5839cf5c79368a80f8ba7b3bf48ffcc
SpiralV <MVelasco90@hotmail.com>
2021-07-14 15:50:36 -0700

    current event status

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,26 +1,52 @@
 // import { useState, useEffect } from "react"
 import { Redirect } from 'react-router-dom'
+import jwt from 'jsonwebtoken'
 import axios from 'axios'
 import Login from'./Login'
 import { useState } from 'react'
-export default function Events(props) {
 
+export default async function Events  (e) {
+    const [eventName, setEventName] = useState('')
+    const [location, setLocation] = useState('')
+    const [friend, setFriend] = useState('')
+    try {
+        e.preventDefault()
+        const requestBody = { eventName, location, friend }
+        const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/events`, requestBody)
+        
+    } catch (err){
+        console.log(err)        
+    }
     return (
-       
-            <div className="log-box">
-                <div className="event-box">
-                    <div>2:30pm</div>
-                    <p>Post-Lunch Check-In</p>
+        <main>
+            <h3 className="new-event-head">Enter a new event</h3>
+            <form onSubmit={Events}>
+                <div>
+                    <input
+                    id='event-input'
+                    type='text'
+                    placeholder='event name'
+                    onChange={(e) => setEventName(e.target.value)}
+                    value={eventName}
+                    />
+                    <input
+                    id='location-input'
+                    type='text'
+                    placeholder='event venue'
+                    onChange={(e) => setLocation(e.target.value)}
+                    value={location}
+                    />
+                    <input
+                    id='zone-input'
+                    type='text'
+                    placeholder='zone name'
+                    onChange={(e) => setFriend(e.target.value)}
+                    value={friend}
+                    />
                 </div>
-                <div className="event-box">
-                    <div>5:00pm</div>
-                    <p>VIP Dinner Check-In</p>
-                </div>
-                <div className="event-box">
-                    <div>7:30pm</div>
-                    <p>Rufus Du Sol @Stage 7!!</p>
-                </div>
-            </div>
-       
+                <input type='submit' value='add event' />
+            </form>
+
+                </main>
     )
-}
+    }
