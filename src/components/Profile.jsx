fe0654e7928077d05f05629fc36aa50bf506756d
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-14 18:05:13 -0500

    Added axios to Profile.jsx, got friends to render

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,6 +4,26 @@ import axios from 'axios'
 import Login from'./Login'
 
 export default function Profile(props) {
+    const [friends, setFriends] = useState([])
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/profile/${props.currentUser.id}`)
+        .then((response) => {
+            setFriends(response.data.friends)
+            console.log(friends)
+        })
+        .catch((err) => console.log(err))
+    },[])
+
+    let myFriends = friends.map(e => {
+        return (
+            <ul>
+                <li>
+                    {e.name}
+                </li>
+            </ul>
+        )
+    })
 
     if (!props.currentUser) return (
         <Redirect 
@@ -18,7 +38,7 @@ export default function Profile(props) {
             <h1>Hello from Profile</h1>
             <p>{props.currentUser.name}</p>
             <p>{props.currentUser.email}</p>
-            <p>{props.currentUser.friends}</p>
+            {myFriends}
         </div>
     )
 }
