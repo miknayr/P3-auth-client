a21100a9672aba4323d6c4f1662af1f39bf18879
SpiralV <MVelasco90@hotmail.com>
2021-07-15 14:22:17 -0700

    updated fields

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from "react"
+import { useState 
+    //, useEffect 
+        } from "react"
 import { Redirect } from 'react-router-dom'
 // do i need jwt token
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 import axios from 'axios'
 import Login from'./Login'
 // import { useState } from 'react'
 
 export default function Events  (props) {
-    // set to app level pass down to events
+    // set to app level pass down to events?
     const [eventName, setEventName] = useState('')
     const [location, setLocation] = useState('')
     const [friend, setFriend] = useState('')
     // useEffect, maybe higher app level
+    // seems to be submitting without useEffect?
+    // trying to get db to create
     async function handleSubmit(e) {
         try {
             e.preventDefault()
@@ -37,23 +41,23 @@ export default function Events  (props) {
             <form onSubmit={handleSubmit}>
                 <div>
                     <input
-                    id='event-input'
+                    id='meetup-title'
                     type='text'
-                    placeholder='event name'
+                    placeholder='meetup title'
                     onChange={(e) => setEventName(e.target.value)}
                     value={eventName}
                     />
                     <input
                     id='location-input'
                     type='text'
-                    placeholder='event venue'
+                    placeholder='meetup location'
                     onChange={(e) => setLocation(e.target.value)}
                     value={location}
                     />
                     <input
-                    id='zone-input'
+                    id='friends-input'
                     type='text'
-                    placeholder='friends'
+                    placeholder='ping friends'
                     onChange={(e) => setFriend(e.target.value)}
                     value={friend}
                     />
