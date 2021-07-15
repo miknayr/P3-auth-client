236f67ab81e7a0eb0abfab15bc1be349c4c38fd0
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-14 19:00:03 -0500

    Added map markers to home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,8 @@
-import { useState, useEffect } from "react"
+// import { useState, useEffect } from "react"
+import Login from'./Login'
 import { Redirect } from 'react-router-dom'
 
-import axios from 'axios'
-import Login from'./Login'
-
 export default function Home(props) {
-    const [message, setMessage] = useState('')
-    const [friends, setFriends] = useState([])
-
-    useEffect(() => {
-        const getPrivateMessage = async () => {
-        try {
-            const token = localStorage.getItem('jwtToken')
-            const authHeaders = { Authorization: token }
-            const response = await axios.get(
-                `${process.env.REACT_APP_SERVER_URL}/api-v1/users/auth-locked`,
-                { headers: authHeaders }
-            )
-            setMessage(response.data.msg)
-            } catch (err) {
-                props.handleLogout()
-            }
-        }
-        getPrivateMessage()    
-    },[props])
 
     if (!props.currentUser) return (
         <Redirect 
@@ -31,11 +10,17 @@ export default function Home(props) {
             component={ Login } 
             currentUser={ props.currentUser }
         />
-    ) 
+    )
     
     return (
         <div className="map-box">
             <img src='/code-chella-map.png' alt="map"/>
+            <p className="map-tag" id="sahara-tag">Sahara</p>
+            <p className="map-tag" id="stage-tag">Stage</p>
+            <p className="map-tag" id="beer-barn-tag">Barn</p>
+            <div className="fas fa-map-marker-alt" id="sahara"/>
+            <div className="fas fa-map-marker-alt" id="stage"/>
+            <div className="fas fa-map-marker-alt" id="beer-barn"/>
         </div>
     )
 }
