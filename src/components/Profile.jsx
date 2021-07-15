f4f05f7ffdf584213cec74561f81fc29941b4288
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-15 17:05:59 -0500

    Added ability to update user location

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,17 +4,33 @@ import axios from 'axios'
 import Login from'./Login'
 
 export default function Profile(props) {
+    const [placeName, setPlaceName] = useState([])
     const [location, setLocation] = useState([])
 
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/profile/${props.currentUser.id}`)
         .then(response => {
-            setLocation(response.data.location)
-            console.log(location)
+            setLocation(response.data.location[0])
         })
         .catch(err => console.log(err))
-    },[])
+    },[location])
 
+    const updateLocation = async (e) => {
+        try {
+            e.preventDefault()
+            const requestBody = { placeName }
+            await axios.put(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/profile/${props.currentUser.id}`, requestBody)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    
     // REDIRECT ON USER ERROR
     if (!props.currentUser) return (
         <Redirect 
@@ -29,7 +45,22 @@ export default function Profile(props) {
         <div>
             <h2 className="component-header">Profile</h2>
             <h4>Hello, {props.currentUser.name}!</h4>
+            <p>{location.name}</p>
             <hr/>
+            <form onSubmit={updateLocation}>
+                <input
+                    type="text"
+                    placeholder="Where Are You?"
+                    onChange={e => setPlaceName(e.target.value)}
+                    value={placeName}
+                    className="login-input"
+                />
+                <input
+                    type='submit'
+                    value='Update'
+                    className="btn login-input text-center"
+                />
+            </form>
         </div>
     )
 }
