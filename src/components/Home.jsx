3acd17057f17d2e47d7ef59e0f6d1780339b93b5
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-16 12:14:52 -0500

    Debugged delete friend route.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
-import Login from'./Login'
-import { Redirect, Link } from 'react-router-dom'
 import { useState, useEffect } from "react"
+import { Redirect, Link } from 'react-router-dom'
 import axios from 'axios'
+import Login from'./Login'
 
 
 export default function Home(props) {
